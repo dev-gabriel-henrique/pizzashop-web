bc7d9f67409f4849987a4fd64ee52ecf8fde3cb5
Gabriel Henrique <work.gabriel12@gmail.com>
2025-01-19 11:13:52 -0300

    finalizado projeto, iniciando testes

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from '@/App'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { enableMSW } from './api/mocks'
+
+enableMSW().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
-// https://vite.dev/config/
+import type { InlineConfig } from 'vitest/node'
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,4 +11,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./src/test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
